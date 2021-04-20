95015b29a4140c39166ec0518a25785e8f6104d3
GitHub Action <action@github.com>
2021-04-20 08:54:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/vvator/ideideafindresourceplugin/MyBundle.kt
+++ src/main/kotlin/com/github/vvator/ideideafindresourceplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vvator.ideideafindresourceplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/vvator/ideideafindresourceplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vvator.ideideafindresourceplugin.services
+
+import com.github.vvator.ideideafindresourceplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
